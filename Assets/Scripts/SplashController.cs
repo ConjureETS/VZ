d5ed3084aa4bf557359f6ba0b740384a38c0b4dd
jparent <jeanp.parent@gmail.com>
2015-08-10 17:04:58 -0400

    add splashscreen with anykey to skip

---
+++ Assets/Scripts/SplashController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SplashController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(AutoSkip());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.anyKeyDown) {
+            Application.LoadLevel(1);
+        }
+	}
+
+    IEnumerator AutoSkip() {
+        
+        yield return new WaitForSeconds(5);
+        Application.LoadLevel(1);
+        
+    }
+}
