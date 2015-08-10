18c585cd29b530917aa97cec193b2ddb0002c07b
Samuel <serenade.124@gmail.com>
2015-08-10 18:29:37 -0400

    - Added scene Sam_unitScene - Updated .gitignore (added thumbs.db.meta and UnityVS project folder) - Added class Unit (not finish) - Added class Command (not finish)

---
+++ Assets/Scripts/Unit.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Unit : MonoBehaviour 
+{
+	public int HP { get; set; }
+	public int Attack { get; set; }
+	public bool isAlly { get; set; }
+	public bool isHuman { get; set; }
+    public bool isDead { get; set; }
+
+	private LinkedList<Command> commandList;
+	// Use this for initialization
+	void Start () 
+	{
+	    // initialize default hp
+        // initialize default attack
+        // initialize default team
+        // initialize default specie
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	    //
+
+	    if (isDead)
+	    {
+	        
+	    }
+	}
+
+    /// <summary>
+    /// Add a command at the end of the list
+    /// </summary>
+    /// <param name="c">The command to add</param>
+	public void addCommand(Command c)
+	{
+		this.commandList.AddLast(c);
+	}
+
+    public void executeCommand()
+    {
+        
+    }
+    
+
+}
