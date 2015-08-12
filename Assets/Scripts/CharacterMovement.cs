581334cbc66065b0ace5e675a897c6a1387a2756
Samuel <serenade.124@gmail.com>
2015-08-12 15:52:01 -0400

    - Added InvalidOperation exception - commented the line enum direction

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -6,9 +6,9 @@ public class CharacterMovement : MonoBehaviour {
     public int player;
 
     public MovementManager mm;
-
-    enum direction { up = "Up",down = "Down", left = "Left", right = "Right", stop = "Stop" };
-
+    // code mis en commentaire par sam, raison : cannot convert from string to int (on ne pouvait pas compiler les scnènes dans unity sans mettre cette ligne en commentaire.)
+    //enum direction { up = "Up",down = "Down", left = "Left", right = "Right", stop = "Stop" };
+    enum direction { up, down, left, right, stop};
     
 
 	// Use this for initialization
