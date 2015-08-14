df305682c85de7e005e19fee737b3ccbc96da1cd
jparent <jeanp.parent@gmail.com>
2015-08-14 14:09:44 -0400

    refactor inputs into a playerController
    add the possibility of changing destination of the camera transition
    before it reaches its destination.

--- Assets/Scripts/SquadCamera.cs
+++ Assets/Scripts/SquadCamera.cs
@@ -3,72 +3,39 @@ using System.Collections;
 
 public class SquadCamera : MonoBehaviour {
 
-    public GameObject[] squads;
-    private GameObject target;
-    public enum Player { Player1, Player2 };
-    public Player player;
-
-    private float y; // store the height value since it will never change
-
-
+    private Vector3 target;
+    public float y = 25f; // store the height value since it will never change
+    
     public float transitionDuration = 2.5f;
-
 
 	// Use this for initialization
 	void Start () {
-        this.target = squads[0];
-
-        this.transform.position= new Vector3( 
-            target.gameObject.transform.position.x,
-            this.transform.position.y,
-            target.gameObject.transform.position.z);
-
-        this.y = this.transform.position.y;
-
+        target = Vector3.zero;
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () {        
         
-        Vector3 previousTarget = target.transform.position;
-
-
-        if(Input.GetAxisRaw("Opt1-1") == 1f){
-            target = squads[0];
-        }
-        else if (Input.GetAxisRaw("Opt2-1") == 1f) {
-            
-            target = squads[1];
-        }
-        else if (Input.GetAxisRaw("Opt3-1") == 1f) {
-            target = squads[2];
-        }
-
-
-        if (!previousTarget.Equals(target.transform.position)) {
-            
-
-            StartCoroutine(Transition());
-
-        }
-
-        
-
-	
 	}
+
+    public void SetTarget(Vector3 newTarget) {
+        Vector3 previousTarget = target;
+        
+        if (!previousTarget.Equals(newTarget)) {
+            
+            target = newTarget;
+            StopCoroutine("Transition");
+            StartCoroutine("Transition");
+        }
+    }
 
 
 
     IEnumerator Transition() {
-        float t = 0.0f; 
-        
-        Vector3 startingPos = transform.position;
-        
-        Vector3 destination = new Vector3(
-                target.transform.position.x,
-                y,
-                target.transform.position.z
-               );
+
+        float t = 0.0f;         
+        Vector3 startingPos = transform.position;        
+        Vector3 destination = new Vector3( target.x, y, target.z );
 
         while (t < 1.0f) {
 
